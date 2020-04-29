cc3d90fed0ba23411dd45b51428b1069c5502200
aculledor <aculledor@gmail.com>
2020-04-29 13:52:13 +0200

    Optimizando AVX MULT

--- Secuencial_Optimizado_AVX/Vectorizar_Multiplicacion/vecMult.c
+++ Secuencial_Optimizado_AVX/Vectorizar_Multiplicacion/vecMult.c
@@ -10,20 +10,9 @@ void inicializarQuaternions(__m256d **A, __m256d **B, int N){
 	
 
 	for(int i=0;i<N;i++){
-		
 		(*A)[i] = _mm256_set_pd((double)rand(),(double)rand(),(double)rand(),(double)rand());
 		(*B)[i] = _mm256_set_pd((double)rand(),(double)rand(),(double)rand(),(double)rand());
 	} 
-	
-	/*printf("\n--------------------------------------\n");
-	
-	for(i=0;i<N;i++){
-		printf("N=%d", i);
-		printf("\n\tA= [%lf, %lf, %lf, %lf]",(*A)[i][0],(*A)[i][1],(*A)[i][2],(*A)[i][3]);
-		printf("\n\tB= [%lf, %lf, %lf, %lf]\n",(*B)[i][0],(*B)[i][1],(*B)[i][2],(*B)[i][3]);
-	}
-	
-	printf("\n## DP [%lf,%lf,%lf,%lf] ##\n",(*DP)[0],(*DP)[1],(*DP)[2],(*DP)[3]);*/
 }
 
 void calculos(__m256d *A, __m256d *B, double **DP, int N){
@@ -39,24 +28,19 @@ void calculos(__m256d *A, __m256d *B, double **DP, int N){
 	}
 	
 	(*DP) = (double *)&result;
-	
-	//printf("\n--------------------------------------\n");
-	
-	//printf("\n## DP [%lf,%lf,%lf,%lf] ##\n",(*DP)[0],(*DP)[1],(*DP)[2],(*DP)[3]);
 }
 
 void darVuelta(double **DP){
 	double aux = 0;
 	aux = (*DP)[0];
-	(*DP)[0] = (*DP)[3];
-	(*DP)[3] = aux;
+		(*DP)[0] = (*DP)[3];
+		(*DP)[3] = aux;
 	aux = (*DP)[1];
-	(*DP)[1] = (*DP)[2];
-	(*DP)[2] = aux;
+		(*DP)[1] = (*DP)[2];
+		(*DP)[2] = aux;
 }
 
 void destruir(__m256d *A, __m256d *B, int N){
-	int i;
 	_mm_free(A);
 	_mm_free(B);
 }
